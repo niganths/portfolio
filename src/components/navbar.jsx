20df220ccdf4e90ea2bd57838c66569d043560e7
Niganth S <115387030+niganths@users.noreply.github.com>
2024-02-02 15:32:13 +0530

    navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -100,11 +100,3 @@ return (
 }
 
 export default Navbar
-
-echo "# portfolio" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/niganths/portfolio.git
-git push -u origin main
